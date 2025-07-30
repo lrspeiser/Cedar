eacf4c345667b3c2f6877bebd8eb7090d545fc2a
Leonard Speiser <leonard@horizon3.net>
2025-07-29 18:28:37 -0700

    Remove notebook module and related files; update dependencies in build configuration.

---
+++ target/debug/deps/dev-84c7668decdd9c60.d
@@ -0,0 +1,5 @@
+/Users/leonardspeiser/Cedar/target/debug/deps/dev-84c7668decdd9c60.d: cedar-core/src/bin/dev.rs
+
+/Users/leonardspeiser/Cedar/target/debug/deps/dev-84c7668decdd9c60: cedar-core/src/bin/dev.rs
+
+cedar-core/src/bin/dev.rs:
